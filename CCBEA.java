f80248b836e8c415150b21cb85c5908710cd9d66
ZERDICORP <56264511+ZERDICORP@users.noreply.github.com>
2022-02-16 15:22:09 +0300

    minor change

--- CCBEA.java
+++ CCBEA.java
@@ -12,7 +12,7 @@ public class CCBEA
 			sum += key[i] * (i + 1);
 		
 		for (int i = 0; i < key.length; ++i)
-			key[i] *= sum * mod;
+			key[i] *= mod * sum;
 
 		for (int i = 0; i < data.length; ++i)
 			data[i] += key[i % key.length];
